bcd6f348885f8c769d1ac25f004f30f9a8e39701
Aditya Kapoor <adityakapoor074@gmail.com>
2023-12-21 23:22:23 +0530

    AREL training params

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -351,7 +351,7 @@ if __name__ == '__main__':
 				"actor_dir": '../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"optimizer_model_path": '../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/optimizer_models/',
+				"optim_dir": '../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/optimizer_models/',
 				"n_epochs": 15,
 				"update_ppo_agent": 30, # update ppo agent after every update_ppo_agent episodes
 				"test_num": test_num,
