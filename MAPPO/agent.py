24cb1611776ae12dfa0be07f1bfe4e89132ebf5f
Aditya Kapoor <adityakapoor074@gmail.com>
2024-01-13 14:39:47 +0530

    update ATRR

--- MAPPO/agent.py
+++ MAPPO/agent.py
@@ -309,7 +309,7 @@ class PPOAgent:
 				episodic_rewards = torch.from_numpy(self.buffer.rewards[self.buffer.episode_num]).float().sum(dim=0)
 	
 				reward_time_wise = episodic_rewards.unsqueeze(-2).to(self.device) * temporal_weights.unsqueeze(-1)
-				print(reward_time_wise)
+				print(reward_time_wise[:, 0])
 
 				return reward_episode_wise
 
