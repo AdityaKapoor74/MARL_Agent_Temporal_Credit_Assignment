0670fc6869c9f0fc03e75ff897327222af696847
Aditya Kapoor <adityakapoor074@gmail.com>
2024-01-23 11:16:43 +0530

    AREL

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -323,7 +323,7 @@ if __name__ == '__main__':
 		extension = "QMix_"+str(i)
 		test_num = "Learning_Reward_Func_for_Credit_Assignment"
 		env_name = "5m_vs_6m"
-		experiment_type = "ATRR" # episodic_team, episodic_agent, temporal_team, temporal_agent, AREL, SeqModel, RUDDER
+		experiment_type = "AREL" # episodic_team, episodic_agent, temporal_team, temporal_agent, AREL, SeqModel, RUDDER
 
 		dictionary = {
 				# TRAINING
