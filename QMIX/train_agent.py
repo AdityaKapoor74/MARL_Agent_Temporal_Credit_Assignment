397a9513a4506ba330d2ab28cd54870e29aa6168
Aditya Kapoor <adityakapoor074@gmail.com>
2024-01-29 00:44:32 +0530

    ATRR LinearNet

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -380,13 +380,13 @@ if __name__ == '__main__':
 				"reward_agent_attn": True,
 				"reward_dropout": 0.0,
 				"reward_attn_net_wide": True,
-				"reward_comp": "hypernet_compression", # no_compression, linear_compression, hypernet_compression
+				"reward_comp": "linear_compression", # no_compression, linear_compression, hypernet_compression
 				"reward_linear_compression_dim": 64,
 				"reward_hypernet_hidden_dim": 64,
 				"reward_hypernet_final_dim": 64,
 				# "num_episodes_capacity": 2000, # 40000
 				"reward_batch_size": 64, # 128
-				"reward_lr": 1e-4,
+				"reward_lr": 1e-3,
 				"reward_weight_decay": 1e-5,
 				"variance_loss_coeff": 0.0,
 				"enable_reward_grad_clip": False,
