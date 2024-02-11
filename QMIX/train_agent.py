908b95736a865f3d8c39a51913c193de5891c80f
Aditya Kapoor <adityakapoor074@gmail.com>
2024-02-11 11:56:57 +0530

    ATRR linear reducing epsilon episodes

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -370,7 +370,7 @@ if __name__ == '__main__':
 				"num_updates": 1,
 				"epsilon_greedy": 1.0,
 				"epsilon_greedy_min": 0.05,
-				"epsilon_greedy_decay_episodes": 30000,
+				"epsilon_greedy_decay_episodes": 20000,
 				"lambda": 0.8,
 
 				# REWARD MODEL
@@ -386,7 +386,7 @@ if __name__ == '__main__':
 				"reward_hypernet_final_dim": 64,
 				# "num_episodes_capacity": 2000, # 40000
 				"reward_batch_size": 64, # 128
-				"reward_lr": 5e-4,
+				"reward_lr": 1e-4,
 				"reward_weight_decay": 1e-5,
 				"variance_loss_coeff": 0.0,
 				"enable_reward_grad_clip": False,
