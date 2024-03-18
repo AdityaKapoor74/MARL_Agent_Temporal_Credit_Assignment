69f8d3fd718416d2c3c0d49c32e5b7bb6c1b2848
Aditya Kapoor <adityakapoor074@gmail.com>
2024-03-18 12:15:01 +0530

    ATRR temporal run

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -348,7 +348,7 @@ if __name__ == '__main__':
 		extension = "QMix_"+str(i)
 		test_num = "Learning_Reward_Func_for_Credit_Assignment"
 		env_name = "5m_vs_6m"
-		experiment_type = "AREL" # episodic_team, episodic_agent, temporal_team, temporal_agent, AREL, ATRR_temporal, ATRR_agent, SeqModel, RUDDER
+		experiment_type = "ATRR_temporal" # episodic_team, episodic_agent, temporal_team, temporal_agent, AREL, ATRR_temporal, ATRR_agent, SeqModel, RUDDER
 
 		dictionary = {
 				# TRAINING
