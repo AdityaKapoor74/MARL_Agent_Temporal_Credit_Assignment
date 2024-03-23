04f89c9445648ba2c925b4bdc98ea5b55ee6a290
Aditya Kapoor <adityakapoor074@gmail.com>
2024-03-23 13:32:33 +0530

    update ATRR QMIX lr 1e-3

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -430,7 +430,7 @@ if __name__ == '__main__':
 				"env": env_name,
 
 				# MODEL
-				"learning_rate": 5e-4, #1e-3
+				"learning_rate": 1e-3, #1e-3
 				"enable_grad_clip": False,
 				"grad_clip": 0.5,
 				"rnn_hidden_dim": 64,
