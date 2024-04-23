608f705e54542b66d5eff35e68f31f5f26ee3e9d
Aditya Kapoor <adityakapoor074@gmail.com>
2024-04-24 00:35:46 +0530

    update codebase ATRR

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -413,7 +413,7 @@ if __name__ == '__main__':
 				"reward_hypernet_final_dim": 64,
 				# "num_episodes_capacity": 2000, # 40000
 				"reward_batch_size": 64, # 128
-				"reward_lr": 1e-5,
+				"reward_lr": 5e-4,
 				"reward_weight_decay": 5e-4,
 				"temporal_score_efficient": 0.0,
 				"agent_score_efficient": 0.0,
