88808debf835823499078717f1ab03cd6b665997
Aditya Kapoor <adityakapoor074@gmail.com>
2024-05-06 13:56:00 +0530

    update baseline

--- QMIX/train_agent.py
+++ QMIX/train_agent.py
@@ -481,8 +481,8 @@ if __name__ == '__main__':
 		extension = "QMix_"+str(i)
 		test_num = "Learning_Reward_Func_for_Credit_Assignment"
 		env_name = "5m_vs_6m"
-		experiment_type = "temporal_team" # episodic_team, episodic_agent, temporal_team, temporal_agent, AREL, ATRR_temporal, ATRR_agent, SeqModel, RUDDER, AREL_agent
-		experiment_name = "IDQN_temporal_team"
+		experiment_type = "episodic_team" # episodic_team, episodic_agent, temporal_team, temporal_agent, AREL, ATRR_temporal, ATRR_agent, SeqModel, RUDDER, AREL_agent
+		experiment_name = "IDQN_episodic_team"
 		dictionary = {
 				# TRAINING
 				"iteration": i,
