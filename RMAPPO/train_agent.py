f0dfa4e0b7ee903a7e4ab79bb2dad349623c06f5
Aditya Kapoor <adityakapoor074@gmail.com>
2024-05-16 16:11:30 +0530

    MAPPO episodic team

--- RMAPPO/train_agent.py
+++ RMAPPO/train_agent.py
@@ -338,8 +338,8 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "Learning_Reward_Func_for_Credit_Assignment"
 		env_name = "5m_vs_6m"
-		experiment_type = "temporal_team" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights
-		experiment_name = "MAPPO_temporal_team"
+		experiment_type = "episodic_team" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights
+		experiment_name = "MAPPO_episodic_team"
 		algorithm_type = "MAPPO" # IPPO, MAPPO
 
 		dictionary = {
