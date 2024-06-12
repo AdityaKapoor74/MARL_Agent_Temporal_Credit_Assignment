bada1c6e9dbc826372a45cf0ca74ee53154082de
Aditya Kapoor <adityakapoor074@gmail.com>
2024-06-13 02:20:28 +0530

    update TAR^2

--- RMAPPO/train_agent.py
+++ RMAPPO/train_agent.py
@@ -402,7 +402,7 @@ if __name__ == '__main__':
 				"variance_loss_coeff": 0.0,
 				"enable_reward_grad_clip": True,
 				"reward_grad_clip_value": 10.0,
-				"replay_buffer_size": 15000,
+				"replay_buffer_size": 5000,
 				"update_reward_model_freq": 200, # 200
 				"reward_model_update_epochs": 400, # 400
 				"norm_rewards": False,
