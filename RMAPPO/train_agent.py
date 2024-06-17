1f3a33b8d17f066f5df36e98de1b903fb25004a6
Aditya Kapoor <adityakapoor074@gmail.com>
2024-06-18 03:33:14 +0530

    update TAR^2

--- RMAPPO/train_agent.py
+++ RMAPPO/train_agent.py
@@ -426,7 +426,7 @@ if __name__ == '__main__':
 				"rnn_hidden_q": 64,
 				"q_value_lr": 5e-4, #1e-3
 				"temperature_q": 1.0,
-				"q_weight_decay": 5e-4,
+				"q_weight_decay": 0.0,
 				"enable_grad_clip_critic_q": True,
 				"grad_clip_critic_q": 10.0,
 				"value_clip": 0.2,
@@ -447,7 +447,7 @@ if __name__ == '__main__':
 				"grad_clip_actor": 10.0,
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
-				"policy_weight_decay": 5e-4,
+				"policy_weight_decay": 0.0,
 				"entropy_pen": 1e-2, #8e-3
 				"entropy_pen_final": 1e-2,
 				"entropy_pen_steps": 20000,
