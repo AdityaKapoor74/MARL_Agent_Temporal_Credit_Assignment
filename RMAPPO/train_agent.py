b1e098750a4b723cb0ee88ab49c67b8dfccb4c82
Aditya Kapoor <adityakapoor074@gmail.com>
2024-06-20 11:13:16 +0530

    temporal team

--- RMAPPO/train_agent.py
+++ RMAPPO/train_agent.py
@@ -352,8 +352,8 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "Learning_Reward_Func_for_Credit_Assignment"
 		env_name = "5m_vs_6m"
-		experiment_type = "ATRR_agent_temporal_attn_weights" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights
-		experiment_name = "IPPO_ATRR_agent_temporal_attn_weights"
+		experiment_type = "temporal_team" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights
+		experiment_name = "IPPO_temporal_team"
 		algorithm_type = "IPPO" # IPPO, MAPPO, IAC, MAAC
 
 		dictionary = {
@@ -397,7 +397,7 @@ if __name__ == '__main__':
 
 
 				# REWARD MODEL
-				"use_reward_model": True,
+				"use_reward_model": False,
 				"reward_n_heads": 3, # 3
 				"reward_depth": 3, # 3
 				"reward_agent_attn": True,
