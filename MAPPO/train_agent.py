1beb98d50b4276f8e588f0b798664770c9b6adb9
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-09 12:28:51 +0530

    MAPPO TAR^2

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -361,8 +361,8 @@ if __name__ == '__main__':
 		environment = "StarCraft" # StarCraft/ Alice_And_Bob
 		env_name = "5m_vs_6m" # 5m_vs_6m/ 10m_vs_11m/ 3s5z/ Alice_And_Bob
 		experiment_type = "ATRR_agent_temporal_attn_weights" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights
-		experiment_name = "IPPO_ATRR_agent_temporal_attn_weights"
-		algorithm_type = "IPPO"
+		experiment_name = "MAPPO_ATRR_agent_temporal_attn_weights"
+		algorithm_type = "MAPPO"
 
 		dictionary = {
 				# TRAINING
