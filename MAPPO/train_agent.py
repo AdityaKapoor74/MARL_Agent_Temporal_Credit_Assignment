8edb0eb2f4145d7a15ac23e6d1cfcf256e7679ab
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-16 14:14:32 +0530

    update TAR^2

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -433,7 +433,7 @@ if __name__ == '__main__':
 				"clamp_rewards": False,
 				"clamp_rewards_value_min": 0.0,
 				"clamp_rewards_value_max": 2.0,
-				"warm_up_period": 0, # 2000
+				"warm_up_period": 2000, # 2000
 
 
 				# REWARD MODEL
