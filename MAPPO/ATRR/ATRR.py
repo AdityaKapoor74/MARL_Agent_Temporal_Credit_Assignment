59b1d1915372286f7f74857cb4875d71213122e2
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-17 19:08:00 +0530

    update

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -263,17 +263,17 @@ class Time_Agent_Transformer(nn.Module):
 		
 		# self.pre_final_norm = nn.LayerNorm(self.comp_emb*depth)
 
-		self.rblocks = nn.Sequential(
-			init_(nn.Linear(16*3*depth*2, 1), activate=False),
-		# 	# init_(nn.Linear(16*4*depth, self.comp_emb), activate=True),
-		# 	# nn.GELU(),
-		# 	# init_(nn.Linear(self.comp_emb, self.comp_emb), activate=True),
-		# 	# nn.GELU(),
-		# 	# init_(nn.Linear(self.comp_emb, 1)),
-		# 	nn.ReLU(),
-			)
+		# self.rblocks = nn.Sequential(
+		# 	init_(nn.Linear(16*3*depth*2, 1), activate=False),
+		# # 	# init_(nn.Linear(16*4*depth, self.comp_emb), activate=True),
+		# # 	# nn.GELU(),
+		# # 	# init_(nn.Linear(self.comp_emb, self.comp_emb), activate=True),
+		# # 	# nn.GELU(),
+		# # 	# init_(nn.Linear(self.comp_emb, 1)),
+		# # 	nn.ReLU(),
+		# 	)
 
-		# self.rblocks = HyperNetwork(obs_dim=16*3*depth, hidden_dim=64, final_state_dim=16*3*depth)
+		self.rblocks = HyperNetwork(obs_dim=16*3*depth, hidden_dim=64, final_state_dim=16*3*depth)
 					   
 		self.do = nn.Dropout(dropout)
 
@@ -351,8 +351,8 @@ class Time_Agent_Transformer(nn.Module):
 			all_x = torch.cat(x_intermediate, dim=-1).reshape(b, n_a, t, -1)
 			indiv_agent_episode_len = (agent_masks.sum(dim=-2)-1).unsqueeze(-1).unsqueeze(-1).expand(-1, -1, -1, 16*3*self.depth).long() # subtracting 1 for indexing purposes
 			final_x = torch.gather(all_x, 2, indiv_agent_episode_len).mean(dim=1, keepdims=True)
-			# rewards = self.rblocks(all_x.reshape(b*n_a*t, -1), final_x.reshape(b, 1, -1).repeat(1, n_a*t, 1).reshape(b*n_a*t, -1)).reshape(b, n_a, t).transpose(1, 2) * agent_masks.to(self.device)
-			rewards = self.rblocks(torch.cat([all_x, final_x.reshape(b, 1, 1, -1).repeat(1, self.n_agents, t, 1)], dim=-1)).transpose(1, 2).squeeze(-1) * agent_masks.to(self.device)
+			rewards = self.rblocks(all_x.reshape(b*n_a*t, -1), final_x.reshape(b, 1, -1).repeat(1, n_a*t, 1).reshape(b*n_a*t, -1)).reshape(b, n_a, t).transpose(1, 2) * agent_masks.to(self.device)
+			# rewards = self.rblocks(torch.cat([all_x, final_x.reshape(b, 1, 1, -1).repeat(1, self.n_agents, t, 1)], dim=-1)).transpose(1, 2).squeeze(-1) * agent_masks.to(self.device)
 		else:
 			
 			indiv_agent_episode_len = (agent_masks.sum(dim=-2)-1).unsqueeze(-1).unsqueeze(-1).expand(-1, -1, -1, 16*3*self.depth).long() # subtracting 1 for indexing purposes
