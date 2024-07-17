65122acaa4fa79f51f9989a4b4b97eb7b6f4fb27
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-18 02:23:41 +0530

    update TAR^2

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -453,8 +453,8 @@ if __name__ == '__main__':
 				"enable_reward_grad_clip": True,
 				"reward_grad_clip_value": 0.5,
 				"replay_buffer_size": 5000,
-				"update_reward_model_freq": 100, # 200
-				"reward_model_update_epochs": 200, # 400
+				"update_reward_model_freq": 10, # 200
+				"reward_model_update_epochs": 5, # 400
 				"norm_rewards": False,
 
 
