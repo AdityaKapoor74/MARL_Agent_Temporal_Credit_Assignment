bee4cca06c74dfafc042b5df31f1f1f56e785f38
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-19 02:08:13 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -264,16 +264,14 @@ class Time_Agent_Transformer(nn.Module):
 		# self.pre_final_norm = nn.LayerNorm(self.comp_emb*depth)
 
 		self.rblocks = nn.Sequential(
-			init_(nn.Linear(self.comp_emb*3*depth*2, self.comp_emb), activate=False),
-			nn.GELU(),
-			init_(nn.Linear(self.comp_emb, 1), activate=False),
+			init_(nn.Linear(self.comp_emb*3*depth*2, 1), activate=False),
 			# init_(nn.Linear(16*3*depth*2, self.comp_emb), activate=True),
 			# nn.GELU(),
 			# init_(nn.Linear(self.comp_emb, self.comp_emb), activate=True),
 			# nn.GELU(),
 			# init_(nn.Linear(self.comp_emb, 1)),
 			# nn.ReLU(),
-			nn.Tanh()
+			# nn.Tanh()
 			)
 
 		# self.projection = nn.Sequential(
@@ -359,7 +357,7 @@ class Time_Agent_Transformer(nn.Module):
 			indiv_agent_episode_len = (agent_masks.sum(dim=-2)-1).unsqueeze(-1).unsqueeze(-1).expand(-1, -1, -1, 16*3*self.depth).long() # subtracting 1 for indexing purposes
 			final_x = torch.gather(all_x, 2, indiv_agent_episode_len).mean(dim=1, keepdims=True)
 			# rewards = self.rblocks(all_x.reshape(b*n_a*t, -1), final_x.reshape(b, 1, -1).repeat(1, n_a*t, 1).reshape(b*n_a*t, -1)).reshape(b, n_a, t).transpose(1, 2) * agent_masks.to(self.device)
-			rewards = self.rblocks(torch.cat([all_x, final_x.reshape(b, 1, 1, -1).repeat(1, self.n_agents, t, 1)], dim=-1)).transpose(1, 2).squeeze(-1) * agent_masks.to(self.device)
+			rewards = (self.rblocks(torch.cat([all_x, final_x.reshape(b, 1, 1, -1).repeat(1, self.n_agents, t, 1)], dim=-1)).transpose(1, 2).squeeze(-1) * agent_masks.to(self.device)).clip(0, 1.0)
 			
 			# all_x = torch.cat(x_intermediate, dim=-1).reshape(b, n_a, t, -1)
 			# all_x = self.projection(all_x)
