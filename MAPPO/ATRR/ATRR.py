2280665d1608a5c83912a83999fb10278e698a4f
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-19 00:12:06 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -263,20 +263,22 @@ class Time_Agent_Transformer(nn.Module):
 		
 		# self.pre_final_norm = nn.LayerNorm(self.comp_emb*depth)
 
-		# self.rblocks = nn.Sequential(
-		# 	init_(nn.Linear(self.comp_emb*3*depth*2, 1), activate=False),
-		# 	# init_(nn.Linear(16*3*depth*2, self.comp_emb), activate=True),
-		# 	# nn.GELU(),
-		# 	# init_(nn.Linear(self.comp_emb, self.comp_emb), activate=True),
-		# 	# nn.GELU(),
-		# 	# init_(nn.Linear(self.comp_emb, 1)),
-		# 	# nn.ReLU(),
-		# 	nn.Sigmoid()
-		# 	)
-
-		self.projection = nn.Sequential(
-			init_(nn.Linear(self.comp_emb*3*depth, self.comp_emb), activate=False),
+		self.rblocks = nn.Sequential(
+			init_(nn.Linear(self.comp_emb*3*depth*2, self.comp_emb), activate=False),
+			nn.GELU(),
+			init_(nn.Linear(self.comp_emb, 1), activate=False),
+			# init_(nn.Linear(16*3*depth*2, self.comp_emb), activate=True),
+			# nn.GELU(),
+			# init_(nn.Linear(self.comp_emb, self.comp_emb), activate=True),
+			# nn.GELU(),
+			# init_(nn.Linear(self.comp_emb, 1)),
+			# nn.ReLU(),
+			nn.Sigmoid()
 			)
+
+		# self.projection = nn.Sequential(
+		# 	init_(nn.Linear(self.comp_emb*3*depth, self.comp_emb), activate=False),
+		# 	)
 
 		# self.rblocks = HyperNetwork(obs_dim=16*3*depth, hidden_dim=64, final_state_dim=16*3*depth)
 					   
