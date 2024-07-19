3dc09562113067ad9d05f150424f86acdf265c95
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-19 19:25:39 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -275,7 +275,9 @@ class Time_Agent_Transformer(nn.Module):
 			# )
 
 		self.projection = nn.Sequential(
-			init_(nn.Linear(self.comp_emb*3*depth, self.comp_emb), activate=False),
+			init_(nn.Linear(self.comp_emb*3*depth, self.comp_emb), activate=True),
+			nn.GELU(),
+			init_(nn.Linear(self.comp_emb, self.comp_emb), activate=False),
 			)
 
 		# self.rblocks = HyperNetwork(obs_dim=16*3*depth, hidden_dim=64, final_state_dim=16*3*depth)
@@ -354,7 +356,7 @@ class Time_Agent_Transformer(nn.Module):
 			# all_ally_enemy_obs = torch.cat([ally_obs.transpose(1, 2).reshape(b, t, -1), enemy_obs.transpose(1, 2).reshape(b, t, -1)], dim=-1)
 			# final_state = torch.gather(all_ally_enemy_obs, 1, (team_masks.sum(dim=-1)-1).unsqueeze(-1).unsqueeze(-1).expand(-1, -1, all_ally_enemy_obs.shape[-1]).long()).squeeze(1)
 			all_x = torch.cat(x_intermediate, dim=-1).reshape(b, n_a, t, -1)
-			all_x = self.projection(all_x)
+			# all_x = self.projection(all_x)
 			indiv_agent_episode_len = (agent_masks.sum(dim=-2)-1).unsqueeze(-1).unsqueeze(-1).expand(-1, -1, -1, self.comp_emb).long() # subtracting 1 for indexing purposes
 			final_x = torch.gather(all_x, 2, indiv_agent_episode_len).mean(dim=1, keepdims=True)
 			# rewards = self.rblocks(all_x.reshape(b*n_a*t, -1), final_x.reshape(b, 1, -1).repeat(1, n_a*t, 1).reshape(b*n_a*t, -1)).reshape(b, n_a, t).transpose(1, 2) * agent_masks.to(self.device)
