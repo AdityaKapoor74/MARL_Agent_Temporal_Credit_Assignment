a2aa606a3551ebd1eb82d1609f38110cd0da16f3
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-20 00:31:11 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -264,12 +264,12 @@ class Time_Agent_Transformer(nn.Module):
 		# self.pre_final_norm = nn.LayerNorm(self.comp_emb*depth)
 
 		self.rblocks = nn.Sequential(
-			# init_(nn.Linear(self.comp_emb*3*depth*2, 1), activate=False),
-			init_(nn.Linear(self.comp_emb*3*depth*2, self.comp_emb), activate=True),
-			nn.GELU(),
+			init_(nn.Linear(self.comp_emb*3*depth*2, 1), activate=False),
+			# init_(nn.Linear(self.comp_emb*3*depth*2, self.comp_emb), activate=True),
+			# nn.GELU(),
 			# init_(nn.Linear(self.comp_emb, self.comp_emb), activate=True),
 			# nn.GELU(),
-			init_(nn.Linear(self.comp_emb, 1)),
+			# init_(nn.Linear(self.comp_emb, 1)),
 			# nn.ReLU(),
 			nn.Sigmoid()
 			)
