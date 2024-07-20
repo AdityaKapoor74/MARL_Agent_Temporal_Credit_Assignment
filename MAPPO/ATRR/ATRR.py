78de3e06952458e3ee142622136c931defb624e7
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-20 16:11:54 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -356,7 +356,7 @@ class Time_Agent_Transformer(nn.Module):
 			i += 1
 
 			# keep current context
-			# x = x + state_action_embedding
+			x = x + state_action_embedding
 
 			if self.agent_attn:
 				# odd numbers have agent attention
@@ -367,7 +367,7 @@ class Time_Agent_Transformer(nn.Module):
 				i += 1
 
 				# keep current context
-				# x = x + state_action_embedding
+				x = x + state_action_embedding
 
 
 			x_intermediate.append(x)
