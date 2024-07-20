9856e95c83023d8db115a65a39e135605d8e52de
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-20 18:57:27 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -177,9 +177,10 @@ class ReturnMixNetwork(nn.Module):
 		# 	)
 
 		self.hyper_w1 = nn.Sequential(
-			init_(nn.Linear(total_obs_dim, hidden_dim), activate=True),
-			nn.GELU(),
-			init_(nn.Linear(hidden_dim, num_agents))
+			# init_(nn.Linear(total_obs_dim, hidden_dim), activate=True),
+			# nn.GELU(),
+			# init_(nn.Linear(hidden_dim, num_agents))
+			init_(nn.Linear(total_obs_dim, num_agents), activate=False),
 			)
 
 	def forward(self, q_values, total_obs, agent_masks):
