998e476b0fc02b2e98e359ad4309be4081c7a4d8
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-21 14:46:09 +0530

    update STAS 5m_vs_6m

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -391,13 +391,13 @@ if __name__ == '__main__':
 	torch.set_printoptions(profile="full")
 	torch.autograd.set_detect_anomaly(True)
 
-	for i in range(1, 6):
+	for i in range(1, 2):
 		extension = "MAPPO_"+str(i)
 		test_num = "Learning_Reward_Func_for_Credit_Assignment"
 		environment = "StarCraft" # StarCraft/ Alice_and_Bob
 		env_name = "5m_vs_6m" # 5m_vs_6m/ 10m_vs_11m/ 3s5z/ Alice_and_Bob
-		experiment_type = "STAS_agent_temporal" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights, STAS_agent_temporal
-		experiment_name = "MAPPO_STAS_agent_temporal"
+		experiment_type = "ATRR_agent_temporal_attn_weights" # episodic_team, episodic_agent, temporal_team, temporal_agent, uniform_team_redistribution, ATRR_temporal ~ AREL, ATRR_temporal_v2, ATRR_temporal_attn_weights, ATRR_agent, ATRR_agent_temporal_attn_weights, STAS_agent_temporal
+		experiment_name = "MAPPO_ATRR_agent_temporal_attn_weights"
 		algorithm_type = "MAPPO"
 
 		dictionary = {
@@ -444,7 +444,7 @@ if __name__ == '__main__':
 				"reward_agent_attn": True,
 				"reward_dropout": 0.0,
 				"reward_attn_net_wide": True,
-				"version": "agent_temporal", # temporal, temporal_v2, agent_temporal, temporal_attn_weights, agent_temporal_attn_weights
+				"version": "agent_temporal_attn_weights", # temporal, temporal_v2, agent_temporal, temporal_attn_weights, agent_temporal_attn_weights
 				"reward_linear_compression_dim": 16, # 16 for ATRR_agent_temporal
 				"reward_batch_size": 64, # 128
 				"reward_lr": 5e-4,
