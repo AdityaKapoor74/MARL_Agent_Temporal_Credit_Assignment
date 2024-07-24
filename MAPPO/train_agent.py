7e85c7a41cb8e3288fe48a684788dbf2833b0210
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-24 16:06:53 +0530

    temporal team 3s5z

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -475,7 +475,7 @@ if __name__ == '__main__':
 				"v_comp_emb_shape": 64,
 				"enable_grad_clip_critic_v": True,
 				"grad_clip_critic_v": 0.5,
-				"value_clip": 0.05,
+				"value_clip": 0.2,
 				"enable_hard_attention": False,
 				"target_calc_style": "GAE", # GAE, N_steps
 				"n_steps": 5,
@@ -492,7 +492,7 @@ if __name__ == '__main__':
 				"rnn_hidden_actor": 64,
 				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 0.5,
-				"policy_clip": 0.05,
+				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
 				"entropy_pen": 1e-3, #8e-3
