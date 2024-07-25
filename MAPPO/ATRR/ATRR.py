27ed7092a3d9b6a6be6c2380ab1d1e0f3130066e
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-25 11:53:38 +0530

    TAR^2 5m_vs_6m

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -234,7 +234,7 @@ class ReturnMixNetwork(nn.Module):
 
 		w1 = w1.reshape(-1, self.num_agents) * agent_masks.reshape(-1, self.num_agents).to(all_agent_state_action.device)
 		std = torch.normal(torch.zeros_like(expected_rewards.squeeze(1)), torch.ones_like(expected_rewards.squeeze(1))).to(all_agent_state_action.device)
-		x = F.relu(expected_rewards.squeeze(1) + (std*w1)) * agent_masks.reshape(-1, self.num_agents).to(all_agent_state_action.device)
+		x = F.relu(expected_rewards.squeeze(1) + (std*torch.exp(w1))) * agent_masks.reshape(-1, self.num_agents).to(all_agent_state_action.device)
 		
 		# scaling the expected reward
 		# w1 = torch.abs(w1.reshape(-1, self.num_agents, 1) * agent_masks.reshape(-1, self.num_agents, 1).to(all_agent_intermediate_final_state_action.device))
