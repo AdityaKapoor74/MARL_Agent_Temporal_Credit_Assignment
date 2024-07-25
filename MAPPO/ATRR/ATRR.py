f348fd10d230b5a2f273ba833903570d345fa794
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-25 17:25:04 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -408,7 +408,7 @@ class Time_Agent_Transformer(nn.Module):
 
 		first_state_embedding = (state_action_embedding.view(b, n_a, t, self.comp_emb) - action_embedding).reshape(b, n_a, t, 1, self.comp_emb)[:, :, 0, :, :].unsqueeze(2).repeat(1, 1, 1, self.depth, 1).reshape(b, n_a, 1, -1)
 		state_embeddings = (state_action_embedding.view(b, n_a, t, self.comp_emb) - action_embedding).reshape(b, n_a, t, 1, self.comp_emb).sum(dim=1, keepdim=True).repeat(1, n_a, 1, self.depth, 1).reshape(b, n_a, t, -1) / (agent_masks.transpose(1, 2).sum(dim=1, keepdim=True).unsqueeze(-1) + 1e-5)
-		state_embeddings = torch.cat([first_state_embedding\.to(self.device), state_embeddings], dim=2)
+		state_embeddings = torch.cat([first_state_embedding.to(self.device), state_embeddings], dim=2)
 		first_past_state_action_embedding = torch.zeros(b, n_a, 1, self.depth*self.comp_emb)
 		past_state_action_embeddings = torch.cat([first_past_state_action_embedding.to(self.device), torch.cat(x_intermediate, dim=-1).reshape(b, n_a, t, -1)[:, :, :-1, :]], dim=-2)
 		state_past_state_action_embeddings = torch.cat([state_embeddings, past_state_action_embeddings], dim=-1)
