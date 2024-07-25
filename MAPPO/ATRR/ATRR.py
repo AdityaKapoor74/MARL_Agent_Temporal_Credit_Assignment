94dff6d4380376adab21958ebc53fe56220b96b6
Aditya Kapoor <adityakapoor074@gmail.com>
2024-07-25 16:34:05 +0530

    update TAR^2

--- MAPPO/ATRR/ATRR.py
+++ MAPPO/ATRR/ATRR.py
@@ -479,7 +479,7 @@ class Time_Agent_Transformer(nn.Module):
 				importance_sampling = torch.exp(((logprobs.to(self.device) - gen_policy_logprobs.to(self.device)) * agent_masks.to(self.device)).clip(min=1e-5, max=10.0))
 			else:
 				# importance_sampling = torch.ones(b, 1, 1).to(self.device)
-				importance_sampling = torch.ones(b, n_a, t).to(self.device)
+				importance_sampling = torch.ones(b, t, n_a).to(self.device)
 			
 
 			returns = self.return_mix_network(rewards * importance_sampling, all_x, final_x.mean(dim=1).unsqueeze(1).repeat(1, t, 1), agent_masks).reshape(b, t, 1) #* episodic_reward.to(self.device).reshape(b, 1, 1)
