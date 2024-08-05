744121aa3261126a82e18b96a03de6c64b2d6641
Aditya Kapoor <adityakapoor074@gmail.com>
2024-08-05 12:47:48 +0530

    TAR^2 5m_vs_6m

--- MAPPO/agent.py
+++ MAPPO/agent.py
@@ -631,7 +631,7 @@ class PPOAgent:
 				# reward_loss = F.mse_loss(rewards.reshape(actions_batch.shape[0], -1).sum(dim=-1), episodic_reward_batch.to(self.device)) #+ 5e-2*(F.mse_loss(state_prediction, state_target, reduction='none') * team_mask_batch.unsqueeze(-1).to(self.device)).sum() / team_mask_batch.sum() #+ 1e-2*(self.classification_loss(action_prediction.reshape(-1, self.num_actions), actions_batch.long().permute(0, 2, 1).reshape(-1).to(self.device)) * agent_masks_batch.reshape(-1).to(self.device)).sum() / (agent_masks_batch.to(self.device).sum() + 1e-5) #+ 1e-4 * entropy_temporal_weights + 1e-4 * entropy_agent_weights
 				reward_prediction_loss = F.mse_loss(returns.reshape(actions_batch.shape[0], -1).sum(dim=-1), episodic_reward_batch.to(self.device))
 				dynamic_loss = (self.classification_loss(action_prediction.reshape(-1, self.num_actions), actions_batch.long().permute(0, 2, 1).reshape(-1).to(self.device)) * agent_masks_batch.reshape(-1).to(self.device)).sum() / (agent_masks_batch.to(self.device).sum() + 1e-5)
-				reward_loss = reward_prediction_loss + self.dynamic_loss_coeffecient * dynamic_loss + 5e-2 * F.huber_loss(expected_importance_sampling_ratio, target_importance_sampling, reduction='sum') / team_mask_batch.sum()
+				reward_loss = reward_prediction_loss + self.dynamic_loss_coeffecient * dynamic_loss #+ 5e-2 * F.huber_loss(expected_importance_sampling_ratio, target_importance_sampling, reduction='sum') / team_mask_batch.sum()
 				# reward_loss = torch.mean(torch.log(torch.cosh(rewards.squeeze(-1) - episodic_reward_batch.to(self.device))))
 				# reward_loss = F.huber_loss(rewards.squeeze(-1).sum(dim=-1), episodic_reward_batch.to(self.device))
 			else:
