c20fb72ba77bf0e386e799cef7ccf0db18e72377
Aditya Kapoor <adityakapoor074@gmail.com>
2024-09-22 01:23:08 +0530

    update

--- MAPPO/train_agent_multi_proc.py
+++ MAPPO/train_agent_multi_proc.py
@@ -706,7 +706,7 @@ if __name__ == '__main__':
 				"enable_reward_grad_clip": True,
 				"reward_grad_clip_value": 0.5,
 				"replay_buffer_size": 5000,
-				"update_reward_model_freq": 100, # 100
+				"update_reward_model_freq": 200, # 100
 				"reward_model_update_epochs": 50, # 200
 				"norm_rewards": False,
 
