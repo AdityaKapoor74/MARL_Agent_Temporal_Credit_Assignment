97705577e06271f1ace74a7418ffd5a87b942155
Aditya Kapoor <adityakapoor074@gmail.com>
2024-09-27 21:24:19 +0530

    update

--- MAPPO/TARR_HindSight/TARR.py
+++ MAPPO/TARR_HindSight/TARR.py
@@ -252,6 +252,7 @@ class Time_Agent_Transformer(nn.Module):
 		past_state_action_embeddings = torch.cat([first_past_state_action_embedding.to(self.device), all_x[:, :, :-1, :]], dim=-2)
 		state_past_state_action_embeddings = torch.cat([state_embeddings, past_state_action_embeddings], dim=-1) # b, n_a, t, -1
 		
+		torch.cuda.empty_cache()
 		x_goal_states = (all_x*agent_masks.transpose(1, 2).unsqueeze(-1)).unsqueeze(-3).repeat(1, 1, t, 1, 1)
 		state_past_state_action_embeddings = (state_past_state_action_embeddings*agent_masks.transpose(1, 2).unsqueeze(-1)).unsqueeze(-2).repeat(1, 1, 1, t, 1)
 		current_context_goal = torch.cat([state_past_state_action_embeddings, x_goal_states], dim=-1) # b, n_a, t, t, -1
