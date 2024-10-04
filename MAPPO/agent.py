f202b3217a20ac4e5ca11f28d63da77c4a42d2b6
Aditya Kapoor <adityakapoor074@gmail.com>
2024-10-04 23:19:03 +0530

    update

--- MAPPO/agent.py
+++ MAPPO/agent.py
@@ -836,8 +836,7 @@ class PPOAgent:
 			agent_masks = 1-torch.from_numpy(self.buffer.indiv_dones[:, :-1, :]).float()
 			action_prediction = self.inverse_dynamic_network(latent_state_actor.to(self.device), latent_state_actor.to(self.device), agent_masks.to(self.device))
 
-			print(action_prediction.shape, actions.shape, self.inverse_dynamic_cross_entropy_loss(action_prediction.reshape(-1, self.num_actions), actions.reshape(-1).long().to(self.device)).shape, agent_masks.reshape(-1).shape, agent_masks.to(self.device).unsqueeze(1).repeat(1, latent_state_actor.shape[1], 1, 1).shape)
-			inverse_dynamic_loss = (self.inverse_dynamic_cross_entropy_loss(action_prediction.reshape(-1, self.num_actions), actions.reshape(-1).long().to(self.device)) * agent_masks.reshape(-1).to(self.device)).sum() / agent_masks.to(self.device).unsqueeze(1).repeat(1, latent_state_actor.shape[1], 1, 1).reshape(-1).sum() + 1e-5
+			inverse_dynamic_loss = (self.inverse_dynamic_cross_entropy_loss(action_prediction.reshape(-1, self.num_actions), actions.reshape(-1).long().to(self.device)) * agent_masks.unsqueeze(1).repeat(1, latent_state_actor.shape[1], 1, 1).reshape(-1).to(self.device)).sum() / agent_masks.to(self.device).unsqueeze(1).repeat(1, latent_state_actor.shape[1], 1, 1).reshape(-1).sum() + 1e-5
 
 			self.inverse_dynamic_optimizer.zero_grad()
 			inverse_dynamic_loss.backward()
