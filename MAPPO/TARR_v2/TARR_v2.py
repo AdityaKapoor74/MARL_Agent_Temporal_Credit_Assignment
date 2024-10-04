9bc56d3684f86240c976f80b1e28d4b93fe3aab7
Aditya Kapoor <adityakapoor074@gmail.com>
2024-10-04 12:01:32 +0530

    update

--- MAPPO/TARR_v2/TARR_v2.py
+++ MAPPO/TARR_v2/TARR_v2.py
@@ -181,18 +181,18 @@ class TARR(nn.Module):
 
 		# normal inverse dynamics model
 		first_state_embedding = (state_action_embedding.view(b, n_a, t, self.emb_dim) - actions_embed).reshape(b, n_a, t, 1, self.emb_dim)[:, :, 0, :, :].reshape(b, n_a, 1, -1)
-		state_embeddings = (state_action_embedding.view(b, n_a, t, self.emb_dim) - actions_embed).reshape(b, n_a, t, self.emb_dim).sum(dim=1, keepdim=True).repeat(1, n_a, 1, 1).reshape(b, n_a, t, -1) / (agent_temporal_mask.transpose(1, 2).sum(dim=1, keepdim=True).unsqueeze(-1) + 1e-5)
-		state_embeddings = torch.cat([first_state_embedding.to(self.device), state_embeddings[:, :, 1:, :]], dim=2)
+		global_state_embeddings = (state_action_embedding.view(b, n_a, t, self.emb_dim) - actions_embed).reshape(b, n_a, t, self.emb_dim).sum(dim=1, keepdim=True).repeat(1, n_a, 1, 1).reshape(b, n_a, t, -1) / (agent_temporal_mask.transpose(1, 2).sum(dim=1, keepdim=True).unsqueeze(-1) + 1e-5)
+		global_state_embeddings = torch.cat([first_state_embedding.to(self.device), global_state_embeddings[:, :, 1:, :]], dim=2)
 		# using agent specific embeddings only
 		# first_past_state_action_embedding = torch.zeros(b, n_a, 1, self.emb_dim)
 		# past_state_action_embeddings = torch.cat([first_past_state_action_embedding.to(self.device), state_action_embedding[:, :, :-1, :]], dim=-2)
-		# state_past_state_action_embeddings = torch.cat([state_embeddings, past_state_action_embeddings], dim=-1)
+		# state_past_state_action_embeddings = torch.cat([global_state_embeddings, past_state_action_embeddings], dim=-1)
 		# using intermediate embeddings
 		first_past_state_action_embedding = torch.zeros(b, n_a, 1, self.n_layer*self.emb_dim)
 		past_state_action_embeddings = torch.cat([first_past_state_action_embedding.to(self.device), x_intermediate[:, :, :-1, :]], dim=-2)
 		last_state_embedding = torch.zeros(b, n_a, 1, self.emb_dim)
-		next_state_embedding = torch.stack([state_embeddings[:, :, 1:, :], last_state_embedding], dim=-2)
-		current_past_next_state_embeddings = torch.cat([state_embeddings, past_state_action_embeddings, next_state_embeddings], dim=-1)
+		next_state_embedding = torch.stack([global_state_embeddings[:, :, 1:, :], last_state_embedding], dim=-2)
+		current_past_next_state_embeddings = torch.cat([global_state_embeddings, past_state_action_embeddings, next_state_embeddings], dim=-1)
 		action_prediction = self.dynamics_model(current_past_next_state_embeddings)
 
 		# Hindsight inverse dynamics model
