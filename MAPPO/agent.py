17973dd34cc5bc8fc69c646444d5a230fa27c846
Aditya Kapoor <adityakapoor074@gmail.com>
2024-10-05 15:31:59 +0530

    update

--- MAPPO/agent.py
+++ MAPPO/agent.py
@@ -832,7 +832,7 @@ class PPOAgent:
 		# first update inverse dynamics model
 		if self.use_inverse_dynamics:
 			latent_state_actor = torch.from_numpy(self.buffer.latent_state_actor).float()
-			b, t, n_a = latent_state_actor.shape
+			b, t, n_a, _ = latent_state_actor.shape
 			agent_masks = 1-torch.from_numpy(self.buffer.indiv_dones[:, :-1, :]).float()
 			upper_triangular_matrix = torch.triu(torch.ones(b*n_a, t, t)).reshape(b, n_a, t, t).permute(0, 2, 3, 1)
 			
