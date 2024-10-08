216efa309382b133795fbeaea6d030095c206769
Aditya Kapoor <adityakapoor074@gmail.com>
2024-10-08 16:12:28 +0530

    update

--- MAPPO/agent.py
+++ MAPPO/agent.py
@@ -646,7 +646,7 @@ class PPOAgent:
 					temporal_weights = F.softmax((rewards*agent_masks_batch).sum(dim=-1, keepdim=True) - 1e9 * (1-(agent_masks_batch.sum(dim=-1, keepdim=True)>0).int()), dim=-2)
 					agent_weights = F.softmax((rewards*agent_masks_batch) - 1e9 * (1-agent_masks_batch), dim=-1) * agent_masks_batch
 					# episodic_rewards = torch.from_numpy(self.buffer.rewards[:, :, 0]).sum(dim=1, keepdim=True).unsqueeze(-1)
-					episodic_rewards = rewards.sum(dim=1, keepdim=True).cpu()
+					episodic_rewards = (rewards*agent_masks_batch).reshape(-1, self.max_time_steps*self.num_agents).sum(dim=1, keepdim=True).unsqueeze(-1).cpu()
 
 					return ((temporal_weights*agent_weights).cpu()*episodic_rewards).numpy()
 
