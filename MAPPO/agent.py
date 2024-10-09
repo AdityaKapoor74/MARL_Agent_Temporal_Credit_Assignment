95e7b6d23f30ca5b4df90c7b99272352ea32b2ad
Aditya Kapoor <adityakapoor074@gmail.com>
2024-10-09 18:09:30 +0530

    update

--- MAPPO/agent.py
+++ MAPPO/agent.py
@@ -654,7 +654,7 @@ class PPOAgent:
 					temporal_rewards_copy[(1-(agent_masks_batch.sum(dim=-1, keepdim=True)>0).int()).bool()] = float('nan')
 					min_temporal_rewards, _ = torch_nanmin(temporal_rewards_copy, dim=-2, keepdim=True)
 					max_temporal_rewards, _ = torch_nanmax(temporal_rewards_copy, dim=-2, keepdim=True)
-					temporal_weights = ((temporal_rewards-min_temporal_rewards) * (1-(agent_masks_batch.sum(dim=-1, keepdim=True)>0).int())) / (max_temporal_rewards-min_temporal_rewards + 1e-5)
+					temporal_weights = ((temporal_rewards-min_temporal_rewards) * (agent_masks_batch.sum(dim=-1, keepdim=True)>0).int()) / (max_temporal_rewards-min_temporal_rewards + 1e-5)
 
 					agent_rewards_copy = copy.deepcopy(rewards)
 					agent_rewards_copy[agent_masks_batch.bool()] = float('nan')
@@ -662,7 +662,9 @@ class PPOAgent:
 					max_agent_rewards, _ = torch_nanmax(agent_rewards_copy, dim=-1, keepdim=True)
 					agent_weights = ((rewards-min_agent_rewards)*agent_masks_batch) / (max_agent_rewards-min_agent_rewards + 1e-5)
 
+					print(temporal_rewards.shape, temporal_weights.shape, min_temporal_rewards.shape, max_temporal_rewards.shape)
 					print(temporal_weights.sum(dim=-2))
+					print(rewards.shape, min_agent_rewards.shape, max_agent_rewards.shape, agent_weights.shape)
 					print(agent_weights.sum(dim=-1))
 
 					# episodic_rewards = torch.from_numpy(self.buffer.rewards[:, :, 0]).sum(dim=1, keepdim=True).unsqueeze(-1)
