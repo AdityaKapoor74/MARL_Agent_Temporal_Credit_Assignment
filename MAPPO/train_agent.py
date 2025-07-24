02533606b1d4b8adcfb8d360a05afdfa29a5d998
Aditya Kapoor <adityakapoor074@gmail.com>
2025-07-24 15:59:34 +0100

    paper ready code

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -184,7 +184,7 @@ class MAPPO:
 						)
 
 				if self.use_reward_model:
-					self.agents.reward_model_buffer.push(
+					self.agents.reward_buffer.push(
 						ally_states, enemy_states, local_obs, global_obs, actions, mask_actions, rnn_hidden_state_actor, action_logprob, rewards_to_send, dones, indiv_dones
 						)
 
@@ -232,7 +232,7 @@ class MAPPO:
 					break
 
 			if self.use_reward_model:
-				self.agents.reward_model_buffer.end_episode()
+				self.agents.reward_buffer.end_episode()
 
 
 			if self.agents.scheduler_need:
@@ -266,7 +266,7 @@ class MAPPO:
 					self.agents.update(episode)
 
 			if self.learn:
-				if self.use_reward_model and self.reward_batch_size <= self.agents.reward_model_buffer.length and episode != 0 and episode % self.update_reward_model_freq == 0:
+				if self.use_reward_model and self.reward_batch_size <= self.agents.reward_buffer.length and episode != 0 and episode % self.update_reward_model_freq == 0:
 					reward_loss_batch, grad_norm_reward_batch = 0.0, 0.0
 					if "AREL" in self.experiment_type:
 						reward_var_batch = 0.0
@@ -275,7 +275,7 @@ class MAPPO:
 						reward_prediction_loss_batch, dynamic_loss_batch = 0.0, 0.0
 					
 					for i in range(self.reward_model_update_epochs):
-						sample = self.agents.reward_model_buffer.sample_reward_model(num_episodes=self.reward_batch_size)
+						sample = self.agents.reward_buffer.sample_reward_model(num_episodes=self.reward_batch_size)
 						if "AREL" in self.experiment_type:
 							reward_loss, reward_var, grad_norm_value_reward = self.agents.update_reward_model(sample)
 							reward_var_batch += (reward_var/self.reward_model_update_epochs)
