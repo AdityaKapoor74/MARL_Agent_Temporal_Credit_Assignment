051845e4e680b47b70d9969ac4f9cdc5d4431d48
Aditya Kapoor <adityakapoor074@gmail.com>
2025-07-25 16:35:12 +0100

    paper ready code

--- MAPPO/train_agent.py
+++ MAPPO/train_agent.py
@@ -374,7 +374,7 @@ if __name__ == '__main__':
 				"version": "temporal", # temporal, agent_temporal ---- For AREL
 				"reward_linear_compression_dim": 64, # 16 for TAR^2_agent_temporal
 				"reward_batch_size": 64, # 128
-				"reward_lr": 5e-4,
+				"reward_lr": 1e-4,
 				"reward_weight_decay": 0.0,
 				"dynamic_loss_coeffecient": 5e-2,
 				"variance_loss_coeff": 0.0,
@@ -412,8 +412,8 @@ if __name__ == '__main__':
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, 
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 0.0, #8e-3
-				"entropy_pen_final": 0.0,
+				"entropy_pen": 8e-3, #8e-3
+				"entropy_pen_final": 8e-3,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
 				"norm_adv": True,
